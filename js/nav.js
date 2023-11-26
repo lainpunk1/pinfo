03a2e63bfc85bcc08140f5562d8d01805b39b32e
Lainpunk1 <lainpunk1@protonmail.com>
2023-11-26 15:15:04 -0300

    menu y loader completos

---
+++ js/nav.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuContainer = document.querySelector(".menu-container");
+    const menuSection2Buttons = document.querySelectorAll(".menu-button-services, .menu-button, .trademark, .menu-section-inner-2");
+    const closeButton = document.querySelector(".menu-section-3");
+
+    let tl = gsap.timeline({ paused: true });
+
+    tl.to(menuContainer, {
+        height: "14vw",
+        duration: 0.75,
+        ease: "power2.out",
+    });
+
+    tl.from([menuSection2Buttons, closeButton], { // Combine both menuSection2Buttons and closeButton
+        opacity: 0,
+        y: 60,
+        stagger: 0.1,
+        duration: 0.75,
+        ease: "power1.inOut",
+    }, "-=0.25");
+
+    function openMenu() {
+        menuContainer.classList.remove("hidden");
+        menuContainer.style.pointerEvents = "all";
+        tl.play();
+    }
+
+    function closeMenu() {
+        tl.reverse().then(() => {
+            menuContainer.classList.add("hidden");
+            menuContainer.style.pointerEvents = "none";
+        });
+    }
+
+    document.querySelector(".menu-icon").addEventListener("click", openMenu);
+    closeButton.addEventListener("click", closeMenu);
+    menuContainer.classList.add("hidden");
+    menuContainer.style.pointerEvents = "none";
+    tl.reverse();
+});
--- js/preloader.js
+++ js/preloader.js
@@ -1,13 +1,93 @@
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-      document.querySelector(
-          "body").style.visibility = "hidden";
-      document.querySelector(
-          "#loader").style.visibility = "visible";
+const text = baffle('.heading-text');  
+const tl = gsap.timeline();
+const welcomeScreen = gsap.timeline({
+  paused: "true",
+});
+
+tl.from(".l-title", {
+  duration: 0.5,
+  opacity: 0,
+  y: 10,
+});
+tl.from(".l-bracket", {
+  duration: 0.3,
+  scale: 0,
+  margin: 0,
+});
+
+tl.from(".l-img", {
+  duration: 0.8,
+  y: 150,
+  opacity: 0,
+  stagger: {
+    amount: 1,
+  },
+});
+tl.from(
+  ".l-bottom-line",
+  {
+    duration: 0.5,
+    y: 50,
+    opacity: 0,
+    stagger: {
+      amount: 0.1,
+    },
+  },
+  "-=.5"
+);
+
+// initializing loader
+let id,
+  i = 0,
+  loadState = false;
+
+function loader() {
+  id = setInterval(frame, 45);
+}
+function frame() {
+  if (i >= 80) {
+    clearInterval(id);
+    welcomeScreen.play();
+    text.start();
+    text.set({
+    characters: '0123456789!@#$%*()®▓▒░ØÆ×±·¨≡µßªƒ',
+    speed: 100
+    });
+    text.reveal(4000);
   } else {
-      document.querySelector(
-          "#loader").style.display = "none";
-      document.querySelector(
-          "body").style.visibility = "visible";
+    if(loadState == true){
+    i++;
+    }
+  }
+}
+window.onload = function () {
+  loader();
+};
+
+// welcome screen
+welcomeScreen.to(".loading-section, .loading-images-container", {
+  y: -10,
+  opacity: 0,
+});
+welcomeScreen.to(".loading-screen", {
+  duration: 0.3,
+  y: -2000,
+  ease: "Power4.out",
+});
+welcomeScreen.from(".img, .heading, .menu, .mask, .pinfo, .inner-inner-container", {
+  duration: 0.6,
+  y: 200,
+  opacity: 0,
+  stagger: {
+    amount: 0.5,
+  },
+});
+
+
+
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+  loadState = true;
   }
 };
